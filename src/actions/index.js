4d8f0cb743848bf88ef5c4f55eb4696fb84d16b6
Nick P Smith <npsmith1990@gmail.com>
2016-06-14 10:29:55 -0500

    show charts, convert kelvins to fahrenheit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
+import axios from 'axios'
 import config from '../../config'
 
 const API_KEY = config.weather_api
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url) // returns a promise
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
